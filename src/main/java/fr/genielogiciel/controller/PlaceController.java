a432207b2c2cae607fa7ee93179437f57923291e
Matthieu Bezard <matthieu.bezard92@gmail.com>
2021-06-13 18:53:48 +0200

    better score calculating

--- src/main/java/fr/genielogiciel/controller/PlaceController.java
+++ src/main/java/fr/genielogiciel/controller/PlaceController.java
@@ -32,28 +32,51 @@ public class PlaceController {
     }
 
     @PostMapping
-    private List<PlaceWithScoreDto> getPlacesFromTags(@RequestParam String tagsAsString) throws JSONException, JsonProcessingException {
+    private List<PlaceWithScoreDto> getPlacesFromTags(
+            @RequestParam String tagsAsString,
+            @RequestParam(required = false) Double latitude,
+            @RequestParam(required = false) Double longitude
+    ) throws JSONException, JsonProcessingException {
         List<Tag> tags = generalService.getObjectListFromJsonString(tagsAsString, Tag.class);
 
-        List<Place> possiblePlaces = placeRepository.findAllByTagsIn(tags);
-        System.out.println(possiblePlaces);
+        List<Place> possiblePlaces = placeRepository.findDistinctByTagsIn(tags);
+        System.out.println("number of possible : " + possiblePlaces.size());
 
         List<Map.Entry<Place, Integer>> placesAndScores = new ArrayList<>();
 
-        possiblePlaces.forEach(place -> placesAndScores.add(new AbstractMap.SimpleEntry<>(place, 0)));
+        for (int i = 0; i < Math.min(31, possiblePlaces.size()); i++) {
+            placesAndScores.add(new AbstractMap.SimpleEntry<>(possiblePlaces.get(i), 0));
+        }
 
         for (Map.Entry<Place, Integer> placesAndScore : placesAndScores) {
+            //Increment score if match with multiple tags
             for (Tag tag : tags) {
                 if (placesAndScore.getKey().getTags().contains(tag)) {
-                    placesAndScore.setValue(placesAndScore.getValue() + 1);
+                    placesAndScore.setValue(placesAndScore.getValue() + 10);
                 }
+            }
 
+            //Multiply score if not too far
+            if (longitude != null && latitude != null) {
+                double distance = Math.sqrt(
+                        Math.pow(latitude - placesAndScore.getKey().getLatitude(), 2) +
+                                Math.pow(longitude - placesAndScore.getKey().getLongitude(), 2)
+                );
+                distance = Math.pow(distance, 1.5) * 10; //make distance more important (optional)
+                double multiplier = Math.max(distance * ((0.1 - 10) / (0.1 - 0)) + 10, 0.1);
+
+                System.out.println("id : " + placesAndScore.getKey().getTitle() +
+                        " distance : " + distance +
+                        " multiplier : " + multiplier +
+                        " score : " + (int) (placesAndScore.getValue() * multiplier));
+
+                placesAndScore.setValue((int) (placesAndScore.getValue() * multiplier));
             }
         }
 
         placesAndScores.sort((o1, o2) -> -o1.getValue().compareTo(o2.getValue()));
         System.out.println(placesAndScores);
-
+        System.out.println("number of final : " + placesAndScores.size());
         return placeConverter.toPlaceWithScoreDto(placesAndScores);
     }
 }
