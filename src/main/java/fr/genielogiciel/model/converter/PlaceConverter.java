3e81ba5b21d9c5da9904785ed32512aca379225b
Charles-Emmanuel <charlesemmanuel.bray@gmail.com>
2021-06-13 17:13:29 +0200

    add place Dto converter

--- src/main/java/fr/genielogiciel/model/converter/PlaceConverter.java
+++ src/main/java/fr/genielogiciel/model/converter/PlaceConverter.java
@@ -1,5 +1,6 @@
 package fr.genielogiciel.model.converter;
 
+import fr.genielogiciel.model.dto.PlaceDto;
 import fr.genielogiciel.model.dto.PlaceWithScoreDto;
 import fr.genielogiciel.model.entity.Place;
 import org.springframework.stereotype.Component;
@@ -28,6 +29,24 @@ public class PlaceConverter {
     public List<PlaceWithScoreDto> toPlaceWithScoreDto(List<Map.Entry<Place, Integer>> places) {
         return places.stream()
                 .map(placeIntegerEntry -> toPlaceWithScoreDto(placeIntegerEntry.getKey(), placeIntegerEntry.getValue()))
+                .collect(Collectors.toList());
+    }
+
+    public PlaceDto toPlaceDto(Place place){
+        PlaceDto placeDto = new PlaceDto();
+        placeDto.setId(place.getId());
+        placeDto.setTags(place.getTags());
+        placeDto.setAddress(place.getAddress());
+        placeDto.setTitle(place.getTitle());
+        placeDto.setLatitude(place.getLatitude());
+        placeDto.setLongitude(place.getLongitude());
+
+        return placeDto;
+    }
+
+    public List<PlaceDto> toPlaceDto(List<Place> places){
+        return places.stream()
+                .map(this::toPlaceDto)
                 .collect(Collectors.toList());
     }
 }
