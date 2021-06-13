09989341ee7972781c4ae8dd178b105768fe8215
edinho <edinhodsr@gmail.com>
2021-06-13 13:12:20 +0200

    back ajout de lieux

--- src/main/java/fr/genielogiciel/controller/PlaceController.java
+++ src/main/java/fr/genielogiciel/controller/PlaceController.java
@@ -6,9 +6,12 @@ import fr.genielogiciel.model.dto.PlaceWithScoreDto;
 import fr.genielogiciel.model.entity.Place;
 import fr.genielogiciel.model.entity.Tag;
 import fr.genielogiciel.model.repository.PlaceRepository;
+import fr.genielogiciel.model.repository.TagRepository;
 import fr.genielogiciel.utils.GeneralService;
 import org.json.JSONException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -23,12 +26,14 @@ public class PlaceController {
     private final GeneralService generalService;
     private final PlaceRepository placeRepository;
     private final PlaceConverter placeConverter;
+    private final TagRepository tagRepository;
 
     @Autowired
-    public PlaceController(GeneralService generalService, PlaceRepository placeRepository, PlaceConverter placeConverter) {
+    public PlaceController(GeneralService generalService, PlaceRepository placeRepository, PlaceConverter placeConverter, TagRepository tagRepository) {
         this.generalService = generalService;
         this.placeRepository = placeRepository;
         this.placeConverter = placeConverter;
+        this.tagRepository = tagRepository;
     }
 
     @PostMapping
@@ -55,5 +60,27 @@ public class PlaceController {
         System.out.println(placesAndScores);
 
         return placeConverter.toPlaceWithScoreDto(placesAndScores);
+    }
+
+    @PostMapping(path = "/addPlace")
+    private ResponseEntity<String> addPlace(
+            @RequestParam String nom,
+            @RequestParam String adresse,
+            @RequestParam String inputTags
+    ){
+
+
+        List<Tag> list = new ArrayList<>();
+
+        if (tagRepository.findByTitle(inputTags).isEmpty()){
+            list.add(tagRepository.findByTitle(inputTags).get());
+        }
+        else{
+            list.add(new Tag(inputTags));
+        }
+
+        Place place = new Place(nom, list, 0.0, 0.0 );
+        placeRepository.save(place);
+        return new ResponseEntity<>("Place added", HttpStatus.OK);
     }
 }
